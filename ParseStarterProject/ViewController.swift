ba541788267d0537b3df5162c1107aca4d4304ec
AugustLE <le_lund@hotmail.com>
2017-02-08 13:10:45 +0100

    Removing of comments

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -1,10 +1,6 @@
 /**
-* Copyright (c) 2015-present, Parse, LLC.
-* All rights reserved.
 *
-* This source code is licensed under the BSD-style license found in the
-* LICENSE file in the root directory of this source tree. An additional grant
-* of patent rights can be found in the PATENTS file in the same directory.
+*
 */
 
 import UIKit
@@ -37,14 +33,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        //testButton.frame.width = testButton.frame.size.
         self.testButton.layer.cornerRadius = self.testButton.frame.size.width/2;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 }
